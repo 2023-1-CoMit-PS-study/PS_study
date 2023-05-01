2731e438e75357a0d864dd88f3d7b68070ac8527
박찬서(Park Chanseo) <101254306+Neoul03@users.noreply.github.com>
2023-05-01 11:17:14 +0900

    Update 5주차.py

--- 박찬서/5주차.py
+++ 박찬서/5주차.py
@@ -2,7 +2,7 @@ def convert(n, base):
     T = "0123456789ABCDEF"
     q, r = divmod(n, base)
 
-    return convert(q, base) + T[r] if q else T[r]
+    return convert(q, base) + T[r] if q else T[r] #결과 = A if 조건 else B, 조건이 True면 A, False면 B
 
 
 def solution(n, t, m, p):
